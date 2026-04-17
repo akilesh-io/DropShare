47a1ba58522e4c6227ea461022b83d2d9f714ffa
akilesh-io <2112akilesh@gmail.com>
2026-04-17 21:32:36 +0530

    package: active atorage package added
    todo: direct file upload feat need to be implemented

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// todo: direct upload instrad of using rails memory
+import * as ActiveStorage from "@rails/activestorage"
+ActiveStorage.start()
+
