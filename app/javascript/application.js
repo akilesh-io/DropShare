37b0d03cfdae08764f3a424b27f6a7d1f61b2d74
akilesh-io <2112akilesh@gmail.com>
2026-05-06 23:33:33 +0530

    refactor: renamed uploads to drop
    - js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "pages/uploads"
+import "pages/drop"
 
 // Controller & Action Name
 const controller = document.body.dataset.controller
