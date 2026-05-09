44148e3730ed761990264b67a81afefb45c7c337
akilesh-io <2112akilesh@gmail.com>
2026-05-10 00:58:17 +0530

    fix: jobs route added before file share

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root "drop#index"
   resources :drop, only: [:index, :new, :create]
 
+  mount MissionControl::Jobs::Engine, at: "/jobs"
   get "/:token",          to: "share#index",    as: :share
   get "/:token/d", to: "share#download", as: :download
 
-  mount MissionControl::Jobs::Engine, at: "/jobs"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
