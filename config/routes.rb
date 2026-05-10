ef7b755eabd8d7c0f1b5b7a72f0023c801f1bd94
akilesh-io <2112akilesh@gmail.com>
2026-05-11 00:13:02 +0530

    fix: moved dynamic route to bottom

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "drop#index"
   resources :drop, only: [:index, :new, :create]
-
   mount MissionControl::Jobs::Engine, at: "/jobs"
-  get "/:token",          to: "share#index",    as: :share
-  get "/:token/d", to: "share#download", as: :download
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  get "/:token",          to: "share#index",    as: :share
+  get "/:token/d", to: "share#download", as: :download
 end
