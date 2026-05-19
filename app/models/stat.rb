103e905c22de316e19b0533a91d52a35a42e5ae4
akilesh-io <2112akilesh@gmail.com>
2026-05-20 00:22:27 +0530

    feat: carbon footprint stats added

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -8,4 +8,17 @@ class Stat < ApplicationRecord
       lifetime_size: 0
     )
   end
+
+  def estimated_co2_grams
+    gb_transferred = self.lifetime_size.to_f / 1.gigabyte
+
+    energy_intensity = 0.06
+    carbon_intensity = 442
+
+    gb_transferred * energy_intensity * carbon_intensity
+  end
+
+  def estimated_co2_kg
+    (estimated_co2_grams / 1000.0).round(2)
+  end
 end
