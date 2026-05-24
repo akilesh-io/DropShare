9b1008a9f27325fdbae6b7813cee138befe52352
akilesh-io <2112akilesh@gmail.com>
2026-05-24 23:41:59 +0530

    fix: handled not present extension type

--- app/helpers/drop_helper.rb
+++ app/helpers/drop_helper.rb
@@ -2,10 +2,10 @@ module DropHelper
   def attachment_icon(blob)
     extension = blob.filename.extension&.downcase
 
-    case extension
+    icon = case extension
     when "mp3", "wav", "flac", "m4a", "aac", "ogg", "opus", "weba", "alac", "aiff", "ape", "wma", "mid", "midi"
       "icons/audio.svg"
-    when "rb", "js", "ts", "tsx", "jsx", "py", "java", "cpp", "c", "cs", "go", "php", "swift", "kt", "rs", "html", "css", "scss", "json", "xml", "yml", "yaml", "sql"
+    when "rb", "ts", "tsx", "jsx", "py", "java", "cpp", "c", "cs", "go", "php", "swift", "kt", "rs", "html", "scss",  "yml", "yaml"
       "icons/code.svg"
     when "doc", "docx", "txt", "rtf", "odt"
       "icons/document.svg"
@@ -19,10 +19,18 @@ module DropHelper
       "icons/pdf-simple.svg"
     when "zip", "rar", "tar", "gz", "bz2", "xz", "7z"
       "icons/zip.svg"
-    when nil
+    when nil , ""
       "icons/empty.svg"
     else
       "icons/#{extension}.svg"
     end
+    asset_exists?(icon) ? icon : "icons/empty.svg"
+  end
+
+
+  private
+
+  def asset_exists?(path)
+    Rails.root.join("app/assets/images", path).exist?
   end
 end
