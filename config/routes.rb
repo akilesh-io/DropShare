77cccf4c34a500c32d12c8aa58ffdc6f589a7d1a
akilesh-io <2112akilesh@gmail.com>
2026-06-18 16:02:18 +0530

    ref: code format

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
       delete :destroy_koppu
     end
   end
-  mount MissionControl::Jobs::Engine, at: "/jobs"
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
   get "/f/:share_key",        to: "share#download_koppu",    as: :download_koppu
   get "/d/:share_key",        to: "share#download_koppurai", as: :download_koppurai
   get "/:share_key",          to: "share#index",             as: :share
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  mount MissionControl::Jobs::Engine, at: "/jobs"
 end
