4794ae8fda1f3b130809f96cdb68212b640f993d
akilesh-io <2112akilesh@gmail.com>
2026-08-02 12:31:01 +0530

    fix: /health & /jobs route added initially to not conflict with application file url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+  mount MissionControl::Jobs::Engine, at: "/jobs"
+
   root "drop#index"
   resources :drop, only: [:index, :new, :create] do
     member do
@@ -7,13 +11,7 @@ Rails.application.routes.draw do
       delete :destroy_koppu
     end
   end
-
   get "/f/:share_key",        to: "share#download_koppu",    as: :download_koppu
   get "/d/:share_key",        to: "share#download_koppurai", as: :download_koppurai
   get "/:share_key",          to: "share#index",             as: :share
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  mount MissionControl::Jobs::Engine, at: "/jobs"
 end
