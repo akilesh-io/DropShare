7cdca3246376481fc94502ecf77b134db193223c
Akilesh <akil@baltimorehealthanalytics.com>
2026-08-02 23:38:23 +0530

    feat: implement cached_preview_url helper for caching image previews

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def cached_preview_url(representation)
+    rails_blob_representation_proxy_path(
+      signed_blob_id: representation.blob.signed_id,
+      variation_key: representation.variation.key,
+      filename: representation.blob.filename
+    )
+  end
 end
